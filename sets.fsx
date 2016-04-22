2e08c56b6348e5e84289871814cef9a1938fb8fb
Andrew Laptev <a.d.laptev@gmail.com>
2016-04-22 19:48:19 +0400

    Update sets.fsx

--- sets.fsx
+++ sets.fsx
@@ -16,5 +16,38 @@ let singletonSet b =
   in (answer:Set)
 
 let b = singletonSet 5
+let c = singletonSet 6
 contains b 5
 contains b 6
+
+let union (a:Set) (b:Set) =  fun c -> (contains b c) || (contains a c)
+let d = union b c
+contains d 5
+contains d 4
+
+let intersect (a: Set) (b: Set) = fun c -> (contains b c) && (contains a c)
+
+
+let e = intersect d c
+contains e 5
+contains e 6
+
+let diff (a:Set) (b:Set) = fun c -> (contains a c) && (not (contains b c))
+
+
+let filter a f = fun c -> (contains a c) && (f c)
+let f = filter d (fun x -> x % 2 = 0)
+
+
+let forAll (a: Set) (f: int -> bool) =
+  let rec forAll' = function
+    1000 -> true
+    | acc -> printf "%d" acc; if (contains a acc) && not (f acc) then false else forAll' (acc+1)
+  forAll' (-1000)
+
+forAll f (fun x -> x = 6)
+
+
+let exists (a: Set) (f: int -> bool) = not (forAll a (fun x -> not (f x)))
+exists d (fun x -> x = 5)
+exists d (fun x -> x = 4)
